6a555ad0c2c1286b2f15305f7ff45257ba24a923
Sérgio Silva <1141074@isep.ipp.pt>
2017-10-21 16:09:20 +0100

    Generating Javadoc

--- cms/build/putnami/extra/gen/pt/isep/cms/client/ContentWidgetView_ContentWidgetViewUiBinderImpl_GenCss_style.java
+++
@@ -1,9 +0,0 @@
-package pt.isep.cms.client;
-
-import com.google.gwt.resources.client.CssResource;
-
-public interface ContentWidgetView_ContentWidgetViewUiBinderImpl_GenCss_style extends CssResource {
-  String name();
-  String description();
-  String contentTitle();
-}
